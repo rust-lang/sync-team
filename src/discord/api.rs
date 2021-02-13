2ff7749afeda00ccf2cdcf10082a5b1036a481a6
technetos <mrgould93@gmail.com>
2021-02-12 23:24:39 -0500

    Sync with teams api

--- src/discord/api.rs
+++ src/discord/api.rs
@@ -1,56 +1,104 @@
 use failure::{bail, Error};
-use log::{debug, info, trace};
+use log::{info, trace};
 use reqwest::{
     header::{self, HeaderValue},
-    Client, Method, RequestBuilder, Response, StatusCode,
+    Client, Method, RequestBuilder, Response,
 };
+use serde_json::json;
 use std::borrow::Cow;
-use std::collections::HashMap;
 
 pub(crate) struct Discord {
     token: String,
-    dry_run: bool,
     client: Client,
 }
 
 impl Discord {
-    pub(crate) fn new(token: String, dry_run: bool) -> Self {
+    pub(crate) fn new(token: String) -> Self {
         Self {
             token,
-            dry_run,
             client: Client::new(),
         }
     }
 
-    pub(crate) fn get_roles(&self, guild_name: &str) -> Result<HashMap<String, String>, Error> {
-        let guilds = self.get_guilds()?;
+    pub(crate) fn get_guild(&self, name: &str) -> Result<Guild, Error> {
+        #[derive(serde::Deserialize)]
+        struct PartialGuild {
+            name: String,
+            id: String,
+        }
 
-        let guild_id = if let Some(id) = guilds.get(guild_name) {
-            id
+        let maybe_partial_guild = self
+            .req(Method::GET, "/v8/users/@me/guilds")?
+            .send()?
+            .json::<Vec<PartialGuild>>()?
+            .into_iter()
+            .find(|guild| guild.name == name);
+
+        let partial_guild = if let Some(partial_guild) = maybe_partial_guild {
+            partial_guild
         } else {
-            bail!("No guild found by name: {}", guild_name);
+            bail!("No guild found by name: {}", &name);
         };
 
-        Ok(self.req(Method::GET, &format!("/guilds/{}/roles", guild_id))?
+        Ok(self
+            .req(Method::GET, &format!("/v8/guilds/{}", partial_guild.id))?
             .send()?
-            .json::<Vec<Role>>()?
-            .into_iter()
-            .fold(HashMap::new(), |mut hash_map, role| {
-                hash_map.insert(role.name, role.id);
-                hash_map
-            }))
+            .json::<Guild>()?)
     }
 
-    fn get_guilds(&self) -> Result<HashMap<String, String>, Error> {
-        Ok(self
-            .req(Method::GET, "/users/@me/guilds")?
-            .send()?
-            .json::<Vec<Guild>>()?
-            .into_iter()
-            .fold(HashMap::new(), |mut hash_map, guild| {
-                hash_map.insert(guild.name, guild.id);
-                hash_map
-            }))
+    pub(crate) fn get_member(
+        &self,
+        member_id: usize,
+        guild_id: &str,
+    ) -> Result<Option<GuildMember>, Error> {
+        let request = || {
+            Ok(self.req(
+                Method::GET,
+                &format!("/v8/guilds/{}/members/{}", guild_id, member_id),
+            )?)
+        };
+
+        with_rate_limiting(request).map(|maybe_res| {
+            if let Some(mut res) = maybe_res {
+                Some(res.json::<GuildMember>().ok()?)
+            } else {
+                None
+            }
+        })
+    }
+
+    pub(crate) fn update_user_roles(
+        &self,
+        guild_id: &str,
+        user_id: usize,
+        roles: &[String],
+    ) -> Result<(), Error> {
+        let request = || {
+            Ok(self
+                .req(
+                    Method::PATCH,
+                    &format!("/v8/guilds/{}/members/{}", guild_id, user_id),
+                )?
+                .json(&json!({ "roles": roles })))
+        };
+        with_rate_limiting(request)?;
+        Ok(())
+    }
+
+    pub(crate) fn update_guild_role(&self, guild_id: &str, role: &Role) -> Result<(), Error> {
+        let request = || {
+            Ok(self
+                .req(
+                    Method::PATCH,
+                    &format!("/v8/guilds/{}/roles/{}", guild_id, &role.id),
+                )?
+                .json(&json!({
+                    "name": role.name,
+                    "color": role.color,
+                })))
+        };
+        with_rate_limiting(request)?;
+        Ok(())
     }
 
     fn req(&self, method: Method, url: &str) -> Result<RequestBuilder, Error> {
@@ -60,6 +108,7 @@ impl Discord {
             Cow::Owned(format!("https://discord.com/api{}", url))
         };
         trace!("http request: {} {}", method, url);
+
         Ok(self
             .client
             .request(method, url.as_ref())
@@ -74,14 +123,68 @@ impl Discord {
     }
 }
 
-#[derive(serde::Deserialize, Debug)]
-struct Guild {
-    id: String,
-    name: String,
+// Discord has [rate limits] on their REST api.
+//
+// When sending repeated requests to the same guild the rate limiting wait times can be longer than
+// a minute.
+//
+// [rate limits]: https://discord.com/developers/docs/topics/rate-limits
+fn with_rate_limiting<F>(f: F) -> Result<Option<Response>, Error>
+where
+    F: Fn() -> Result<RequestBuilder, Error>,
+{
+    use std::str::FromStr;
+    use std::thread;
+    use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+    loop {
+        let res = f()?.send()?;
+
+        match res.status().as_u16() {
+            200 => return Ok(Some(res)),
+            400 => bail!("bad request"),
+            401 => bail!("invalid auth token"),
+            403 => bail!("insufficient permissions"),
+            404 => return Ok(None),
+            429 => {
+                let future_moment = if let Some(header) = res.headers().get("x-ratelimit-reset") {
+                    f64::from_str(header.to_str()?)? as u64
+                } else {
+                    bail!("no x-ratelimit-reset header found in 429 response")
+                };
+
+                let delay = (UNIX_EPOCH + Duration::from_secs(future_moment))
+                    .duration_since(SystemTime::now())?;
+
+                info!("rate limited: delaying for {} seconds", delay.as_secs());
+                thread::sleep(delay);
+            }
+            c => bail!("unexpected status code: {}", c),
+        }
+    }
 }
 
 #[derive(serde::Deserialize, Debug)]
-struct Role {
+pub(crate) struct Guild {
+    pub id: String,
+    pub name: String,
+    pub roles: Vec<Role>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub(crate) struct Role {
+    pub id: String,
+    pub name: String,
+    pub color: usize,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub(crate) struct GuildMember {
+    user: DiscordUser,
+    pub roles: Vec<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub(crate) struct DiscordUser {
     id: String,
-    name: String,
 }
--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -2,30 +2,200 @@ mod api;
 
 use self::api::Discord;
 use crate::TeamApi;
-use failure::Error;
-use log::{debug, info};
+use failure::{bail, Error};
+use log::{info, warn};
+use std::collections::HashMap;
 
-const RUST_LANG_DISCORD: &'static str = "The Rust Programming Language";
-const TEST: &'static str = "Test";
+const RUST_LANG_DISCORD: &str = "The Rust Programming Language";
+const TEST: &str = "Test";
 
 pub(crate) struct SyncDiscord {
     discord: Discord,
+    dry_run: bool,
     teams: Vec<rust_team_data::v1::Team>,
 }
 
 impl SyncDiscord {
     pub(crate) fn new(token: String, team_api: &TeamApi, dry_run: bool) -> Result<Self, Error> {
         let teams = team_api.get_teams()?;
-        let discord = Discord::new(token, dry_run);
 
-        Ok(Self { discord, teams })
+        let discord = Discord::new(token);
+
+        Ok(Self {
+            discord,
+            teams,
+            dry_run,
+        })
     }
 
     pub(crate) fn run(&self) -> Result<(), Error> {
-        dbg!(self.discord.get_roles(TEST)?);
+        let guild = self.discord.get_guild(TEST)?;
 
-        let teams = self.teams.iter().filter(|team| team.name == "community").collect::<Vec<_>>();
-        dbg!(teams);
+        let guild_id = guild.id;
+
+        info!("Fetching users from discord...");
+        let mut users = self.get_users(&guild_id)?;
+
+        info!("Computing user updates...");
+        let mut user_updates = HashMap::new();
+        for (user_id, user) in &users {
+            self.get_user_updates(*user_id, &mut user_updates, &user);
+        }
+
+        info!("Computing role updates...");
+        let mut roles = guild.roles;
+        let role_updates = self.get_role_updates(&roles)?;
+
+        if !self.dry_run {
+            info!("Applying user updates...");
+
+            for (user_id, updates) in user_updates {
+                let user = users.get_mut(&user_id).unwrap();
+
+                let roles = &mut user.roles;
+
+                for update in updates {
+                    match update {
+                        UserUpdate::AddRole(id) => {
+                            roles.push(format!("{}", id));
+                        }
+                        UserUpdate::RemoveRole(id) => {
+                            roles.retain(|role_id| role_id != &format!("{}", id));
+                        }
+                    }
+                }
+
+                self.discord.update_user_roles(&guild_id, user_id, roles)?;
+            }
+
+            info!("Applying role updates...");
+
+            for (role_id, updates) in role_updates {
+                let mut role = roles
+                    .iter_mut()
+                    .find(|role| role.id == format!("{}", role_id))
+                    .unwrap();
+
+                for update in updates {
+                    match update {
+                        RoleUpdate::ChangeColor(color) => {
+                            role.color = color;
+                        }
+                    }
+                }
+
+                self.discord.update_guild_role(&guild_id, &role)?;
+            }
+        }
+
         Ok(())
     }
+
+    fn get_users(&self, guild_id: &str) -> Result<HashMap<usize, api::GuildMember>, Error> {
+        let mut users = HashMap::new();
+
+        let maybe_discord = &self
+            .teams
+            .iter()
+            .find(|team| team.name == "all")
+            .map(|team| team.discord.as_ref())
+            .flatten();
+
+        if let Some(discord) = &maybe_discord {
+            for member in &discord.members {
+                match self.discord.get_member(*member, &guild_id) {
+                    Ok(Some(guild_member)) => {
+                        users.insert(*member, guild_member);
+                    }
+                    Ok(None) => {
+                        warn!("user {} was not found in the guild", member);
+                        continue;
+                    }
+                    Err(res) => return Err(res),
+                }
+            }
+        }
+
+        Ok(users)
+    }
+
+    fn get_user_updates(
+        &self,
+        user_id: usize,
+        user_updates: &mut HashMap<usize, Vec<UserUpdate>>,
+        user: &api::GuildMember,
+    ) {
+        let current_roles = &user.roles;
+
+        for team in &self.teams {
+            let discord_team = if let Some(discord_team) = &team.discord {
+                discord_team
+            } else {
+                continue;
+            };
+
+            let team_members = &discord_team.members;
+            let team_role_id = &discord_team.role_id;
+
+            if team_members.contains(&user_id)
+                && !current_roles.contains(&format!("{}", team_role_id))
+            {
+                user_updates
+                    .entry(user_id)
+                    .or_insert(vec![])
+                    .push(UserUpdate::AddRole(*team_role_id));
+            }
+
+            if current_roles.contains(&format!("{}", team_role_id))
+                && !team_members.contains(&user_id)
+            {
+                user_updates
+                    .entry(user_id)
+                    .or_insert(vec![])
+                    .push(UserUpdate::RemoveRole(*team_role_id));
+            }
+        }
+    }
+
+    fn get_role_updates(
+        &self,
+        roles: &[api::Role],
+    ) -> Result<HashMap<usize, Vec<RoleUpdate>>, Error> {
+        let mut role_updates = HashMap::new();
+
+        for team in &self.teams {
+            let discord_team = if let Some(discord_team) = &team.discord {
+                discord_team
+            } else {
+                continue;
+            };
+
+            let maybe_role = roles
+                .iter()
+                .find(|role| role.id == format!("{}", discord_team.role_id));
+
+            if let (Some(role), Some(color)) = (maybe_role, discord_team.color.as_ref()) {
+                let color_code = usize::from_str_radix(&color[1..], 16)?;
+
+                if color_code != role.color {
+                    role_updates
+                        .entry(discord_team.role_id)
+                        .or_insert(vec![])
+                        .push(RoleUpdate::ChangeColor(color_code));
+                }
+            }
+        }
+
+        Ok(role_updates)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum UserUpdate {
+    AddRole(usize),
+    RemoveRole(usize),
+}
+
+enum RoleUpdate {
+    ChangeColor(usize),
 }
